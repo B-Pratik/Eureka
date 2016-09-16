1083bba4c7b3ac57c16ead7bdfa9bef1443d4004
Pratik Bhurewar <pratik.bhurewar@3plearning.com>
2016-09-16 12:23:24 +0530

    Highlight Effect Added
    -Highlighted list entry
    -Added Dummy element to increase list height
    
    Signed-off-by: Pratik Bhurewar <pratik.bhurewar@3plearning.com>

--- gulp-files/four-square-response-parse.js
+++ gulp-files/four-square-response-parse.js
@@ -3,85 +3,85 @@
  */
 
 module.exports = function (data) {
-	var parsed = [];
-	for (var iterator of data) {
-		var entry = {};
-		var ite   = iterator.venue;
-		if (ite.name) {
-			entry.name = ite.name;
-		} else {
-			//we dont need a business without a name
-			continue;
-		}
+    var parsed = [];
+    for (var iterator of data) {
+        var entry = {};
+        var ite = iterator.venue;
+        if (ite.name) {
+            entry.name = ite.name;
+        } else {
+            //we dont need a business without a name
+            continue;
+        }
 
-		if (ite.location.address) {
-			entry.address = ite.location.address;
-		} else {
-			entry.address = 'NA';
-		}
+        if (ite.location.address) {
+            entry.address = ite.location.address;
+        } else {
+            entry.address = 'NA';
+        }
 
-		if (ite.categories instanceof Array && ite.categories.length > 0) {
-			entry.categories = [];
+        if (ite.categories instanceof Array && ite.categories.length > 0) {
+            entry.categories = [];
 
-			if (ite.categories[0].icon && ite.categories[0].icon.prefix && ite.categories[0].icon.suffix) {
-				entry.photo = ite.categories[0].icon.prefix + '64' + ite.categories[0].icon.suffix;
-			}
+            if (ite.categories[0].icon && ite.categories[0].icon.prefix && ite.categories[0].icon.suffix) {
+                entry.photo = ite.categories[0].icon.prefix + '64' + ite.categories[0].icon.suffix;
+            }
 
-			ite.categories.forEach(function (e) {
-				if (e.name) {
-					entry.categories.push(e.name);
-				}
-			});
-		} else {
-			entry.categories = 'NA';
-		}
+            ite.categories.forEach(function (e) {
+                if (e.name) {
+                    entry.categories.push(e.name);
+                }
+            });
+        } else {
+            entry.categories = 'NA';
+        }
 
-		if (ite.location instanceof Object &&
-			ite.location.lat !== undefined &&
-			ite.location.lng !== undefined) {
-			entry.cords = {lat: ite.location.lat, lon: ite.location.lng};
-		} else {
-			//we dont need a business without coordinates
-			continue;
-		}
+        if (ite.location instanceof Object &&
+            ite.location.lat !== undefined &&
+            ite.location.lng !== undefined) {
+            entry.cords = {lat: ite.location.lat, lon: ite.location.lng};
+        } else {
+            //we dont need a business without coordinates
+            continue;
+        }
 
-		if (ite.location.city) {
-			entry.city = ite.location.city;
-		} else {
-			entry.city = 'NA';
-		}
+        if (ite.location.city) {
+            entry.city = ite.location.city;
+        } else {
+            entry.city = 'NA';
+        }
 
-		if (ite.rating) {
-			entry.rating = ite.rating;
-		} else {
-			entry.rating = 'NA';
-		}
+        if (ite.rating) {
+            entry.rating = (ite.rating / 2).toFixed(1);
+        } else {
+            entry.rating = 'NA';
+        }
 
-		if (ite.contact.phone) {
-			entry.phone = ite.contact.phone;
-		} else {
-			entry.phone = 'NA';
-		}
-
-
-		if (!entry.photo) {
-			entry.photo = 'http://www.megaicons.net/static/img/icons_sizes/8/60/256/science-business-icon.png';
-		}
+        if (ite.contact.phone) {
+            entry.phone = ite.contact.phone;
+        } else {
+            entry.phone = 'NA';
+        }
 
 
-		if (ite.url) {
-			entry.url = ite.url;
-		} else {
-			entry.url = 'NA';
-		}
+        if (!entry.photo) {
+            entry.photo = 'http://www.megaicons.net/static/img/icons_sizes/8/60/256/science-business-icon.png';
+        }
 
-		if (iterator.tips instanceof Array && iterator.tips.length > 0 && iterator.tips[0].text) {
-			entry.description = iterator.tips[0].text;
-		} else {
-			entry.description = 'NA';
-		}
 
-		parsed.push(entry);
-	}
-	return parsed;
+        if (ite.url) {
+            entry.url = ite.url;
+        } else {
+            entry.url = 'NA';
+        }
+
+        if (iterator.tips instanceof Array && iterator.tips.length > 0 && iterator.tips[0].text) {
+            entry.description = iterator.tips[0].text;
+        } else {
+            entry.description = 'NA';
+        }
+
+        parsed.push(entry);
+    }
+    return parsed;
 };
--- source/src-code/controller/app-controller.js
+++ source/src-code/controller/app-controller.js
@@ -8,242 +8,248 @@ import PS  from 'perfect-scrollbar';
 var markerImage = require('../../assets/images/marker.png');
 
 export default function ($scope, $http, $timeout, Notifier) {
-	'ngInject';
+    'ngInject';
 
-	var GoogleMap, mapView;
+    var GoogleMap, mapView;
 
-	var isSearching = false; //flag to know if search is pending
+    var isSearching = false; //flag to know if search is pending
 
-	var markers = [];
+    var markers = [];
 
-	$scope.results = [];
+    $scope.results = [];
 
-	var container = $('.search-list').get(0);
-	PS.initialize(container, {
-		wheelSpeed        : 2,
-		wheelPropagation  : true,
-		minScrollbarLength: 20
-	});
+    var container = $('.search-list').get(0);
+    PS.initialize(container, {
+        wheelSpeed: 2,
+        wheelPropagation: true,
+        minScrollbarLength: 20
+    });
 
-	function initializeMap() {
-		mapView = new GoogleMap.Map(document.getElementById('map-canvas'), {
-			center           : {lat: 0, lng: 0},
-			zoom             : 15,
-			mapTypeId        : GoogleMap.MapTypeId.ROADMAP,
-			styles           : [{
-				featureType: 'poi.business',
-				elementType: 'labels',
-				stylers    : [
-					{visibility: 'off'}
-				]
-			}],
-			zoomControl      : true,
-			scaleControl     : true,
-			streetViewControl: false,
-			mapTypeControl   : false
-		});
-	}
+    function initializeMap() {
+        mapView = new GoogleMap.Map(document.getElementById('map-canvas'), {
+            center: {lat: 0, lng: 0},
+            zoom: 15,
+            mapTypeId: GoogleMap.MapTypeId.ROADMAP,
+            styles: [{
+                featureType: 'poi.business',
+                elementType: 'labels',
+                stylers: [
+                    {visibility: 'off'}
+                ]
+            }],
+            zoomControl: true,
+            scaleControl: true,
+            streetViewControl: false,
+            mapTypeControl: false
+        });
+    }
 
-	function showFirstInput() {
-		let toFocus = $('.show-item input').first();
-		if (toFocus) {
-			toFocus.focus();
-		}
-	}
+    function showFirstInput() {
+        let toFocus = $('.show-item input').first();
+        if (toFocus) {
+            toFocus.focus();
+        }
+    }
 
-	function getData(query, cb) {
-		$http.post('/getdata', query, {timeout: 10000}).then(function (resp) {
-			if (resp.data instanceof Array && resp.data.length > 0) {
-				return cb(null, resp.data);
-			} else if (resp.data instanceof Object && resp.data.error) {
-				Notifier.notify('Error', 'Internal error,try again', 'error');
-				showFirstInput();
-				return cb(true);
-			} else {
-				Notifier.notify('No results', 'try with differnt query', 'error');
-				showFirstInput();
-				return cb(true);
-			}
-		}, function () {
-			Notifier.notify('Unable to get results', 'try again', 'error');
-			showFirstInput();
-			return cb(true);
-		});
-	}
+    function getData(query, cb) {
+        $http.post('/getdata', query, {timeout: 10000}).then(function (resp) {
+            if (resp.data instanceof Array && resp.data.length > 0) {
+                return cb(null, resp.data);
+            } else if (resp.data instanceof Object && resp.data.error) {
+                Notifier.notify('Error', 'Internal error,try again', 'error');
+                showFirstInput();
+                return cb(true);
+            } else {
+                Notifier.notify('No results', 'try with differnt query', 'error');
+                showFirstInput();
+                return cb(true);
+            }
+        }, function () {
+            Notifier.notify('Unable to get results', 'try again', 'error');
+            showFirstInput();
+            return cb(true);
+        });
+    }
 
-	function setMapOnAll(map) {
-		for (var i = 0; i < markers.length; i++) {
-			markers[i].setMap(map);
-		}
-	}
+    function setMapOnAll(map) {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(map);
+        }
+    }
 
-	function toggleBounce(marker) {
-		if (marker.getAnimation() !== null) {
-			marker.setAnimation(null);
-			if (marker.timeout) {
-				clearTimeout(marker.timeout);
-				delete marker.timeout;
-			}
-		} else {
-			marker.setAnimation(GoogleMap.Animation.BOUNCE);
-			marker.timeout = setTimeout(function () {
-				marker.setAnimation(null);
-			}, 3000);
-		}
-	}
+    function toggleBounce(marker) {
+        if (marker.getAnimation() !== null) {
+            marker.setAnimation(null);
+            if (marker.timeout) {
+                clearTimeout(marker.timeout);
+                delete marker.timeout;
+            }
+        } else {
+            marker.setAnimation(GoogleMap.Animation.BOUNCE);
+            marker.timeout = setTimeout(function () {
+                marker.setAnimation(null);
+            }, 3000);
+        }
+    }
 
-	function scrollTo(index) {
-		let top = $('.search-list .entry-' + index).get(0).offsetTop - 35; //added 35 for top padding
-		$('.search-list').animate({
-			scrollTop: top
-		}, 1000);
-		container.top = 0;
-		PS.update(container);
-	}
+    function scrollTo(index) {
+        let top = $('.search-list .entry-' + index).get(0).offsetTop - 35; //added 35 for top padding
+        $('.search-list').animate({
+            scrollTop: top
+        }, 1000);
+        container.top = 0;
+        PS.update(container);
+    }
 
-	function addMarker(feature, timeout) {
+    function highlightEntry(index) {
+        $('.highlighted').removeClass('highlighted');
+        $('.entry-'+index).addClass('highlighted');
+    }
 
-		setTimeout(function () {
-			let marker = new GoogleMap.Marker({
-				position : feature.position,
-				icon     : markerImage,
-				animation: GoogleMap.Animation.DROP,
-				map      : mapView
-			});
+    function addMarker(feature, timeout) {
 
-			let infowindow = new GoogleMap.InfoWindow({
-				content: feature.title
-			});
+        setTimeout(function () {
+            let marker = new GoogleMap.Marker({
+                position: feature.position,
+                icon: markerImage,
+                animation: GoogleMap.Animation.DROP,
+                map: mapView
+            });
 
-			marker.addListener('mouseover', function () {
-				infowindow.open(mapView, marker);
-			});
+            let infowindow = new GoogleMap.InfoWindow({
+                content: feature.title
+            });
 
-			marker.addListener('mouseout', function () {
-				infowindow.close();
-			});
+            marker.addListener('mouseover', function () {
+                infowindow.open(mapView, marker);
+            });
 
-			marker.addListener('click', function () {
-				toggleBounce(marker);
-				scrollTo(timeout / 100);
-			});
+            marker.addListener('mouseout', function () {
+                infowindow.close();
+            });
 
-			markers.push(marker);
-		}, timeout);
+            marker.addListener('click', function () {
+                toggleBounce(marker);
+                scrollTo(timeout / 100);
+                highlightEntry(timeout / 100);
+            });
 
-	}
+            markers.push(marker);
+        }, timeout);
 
-	function setMarkers(result) {
-		//empty marker stack
-		setMapOnAll(null);
-		markers        = [];
-		$scope.results = [];
+    }
 
-		//reset map center
-		mapView.setCenter(new GoogleMap.LatLng(result[0].cords.lat, result[0].cords.lon));
+    function setMarkers(result) {
+        //empty marker stack
+        setMapOnAll(null);
+        markers = [];
+        $scope.results = [];
 
-		for (let i = result.length - 1; i > -1; i--) {
-			let business   = result[i];
-			business.class = i;
+        //reset map center
+        mapView.setCenter(new GoogleMap.LatLng(result[0].cords.lat, result[0].cords.lon));
 
-			let _marker = {
-				position: new GoogleMap.LatLng(business.cords.lat, business.cords.lon),
-				title   : business.name,
-				data    : business
-			};
+        for (let i = result.length - 1; i > -1; i--) {
+            let business = result[i];
+            business.class = i;
 
-			addMarker(_marker, i * 100);
+            let _marker = {
+                position: new GoogleMap.LatLng(business.cords.lat, business.cords.lon),
+                title: business.name,
+                data: business
+            };
 
-			$timeout(function () {
-				$scope.results.unshift(business);
-			}, 100 * (result.length - (i + 1)));
-		}
-	}
+            addMarker(_marker, i * 100);
 
-	function inputListener() {
-		$('.show-item').keyup(function (event) {
-			if (event.keyCode === 13) {
-				var textboxes        = $('.show-item');
-				var currentBoxNumber = textboxes.index(this);
+            $timeout(function () {
+                $scope.results.unshift(business);
+            }, 100 * (result.length - (i + 1)));
+        }
+    }
 
-				if (textboxes[currentBoxNumber + 1] !== null) {
-					var nextBox = $(textboxes[currentBoxNumber + 1]).children();
-					nextBox.focus();
-					nextBox.click();
-				}
+    function inputListener() {
+        $('.show-item').keyup(function (event) {
+            if (event.keyCode === 13) {
+                var textboxes = $('.show-item');
+                var currentBoxNumber = textboxes.index(this);
 
-				event.preventDefault();
-				return false;
-			}
-		});
+                if (textboxes[currentBoxNumber + 1] !== null) {
+                    var nextBox = $(textboxes[currentBoxNumber + 1]).children();
+                    nextBox.focus();
+                    nextBox.click();
+                }
 
-		$('.show-item button').on('click', search);
-	}
+                event.preventDefault();
+                return false;
+            }
+        });
 
-	function resetSearchBar() {
-		let searchBar = $('.search-bar');
-		searchBar.remove();
-		$('body').append(searchBar);
+        $('.show-item button').on('click', search);
+    }
 
-		setTimeout(inputListener, 100);
-	}
+    function resetSearchBar() {
+        let searchBar = $('.search-bar');
+        searchBar.remove();
+        $('body').append(searchBar);
 
-	function search() {
-		let inputs = $('.show-item input');
+        setTimeout(inputListener, 100);
+    }
 
-		let query    = inputs.get(0).value.trim() || 'business',
-		    location = inputs.get(1).value.trim();
+    function search() {
+        let inputs = $('.show-item input');
 
-		resetSearchBar();//reattach element to deal with sticky hover
+        let query = inputs.get(0).value.trim() || 'business',
+            location = inputs.get(1).value.trim();
+
+        resetSearchBar();//reattach element to deal with sticky hover
 
 
-		if (!GoogleMap) {
-			Notifier.notify('Loading content', 'please wait', 'warn');
-			return;
-		}
+        if (!GoogleMap) {
+            Notifier.notify('Loading content', 'please wait', 'warn');
+            return;
+        }
 
-		if (!location) {
-			Notifier.notify('Invalid input', 'please provide location', 'warn');
-			return;
-		}
+        if (!location) {
+            Notifier.notify('Invalid input', 'please provide location', 'warn');
+            return;
+        }
 
-		if (location.length === 0) {
-			Notifier.notify('Invalid input', 'please provide location', 'warn');
-			return;
-		}
+        if (location.length === 0) {
+            Notifier.notify('Invalid input', 'please provide location', 'warn');
+            return;
+        }
 
-		if (isSearching) {
-			Notifier.notify('Please wait,', 'looking for results', 'info');
-			return;
-		} else {
-			isSearching = true;
-		}
+        if (isSearching) {
+            Notifier.notify('Please wait,', 'looking for results', 'info');
+            return;
+        } else {
+            isSearching = true;
+        }
 
-		$('.back-layer').show();
+        $('.back-layer').show();
 
-		getData({query, location}, function (err, results) {
-			isSearching = false;
-			if (!err) {
-				$('.back-layer').hide();
-				$('.search-list').css({'padding-bottom': ($('body').height() - 35) + 'px'});
-				setMarkers(results);
-			}
-		});
-	}
+        getData({query, location}, function (err, results) {
+            isSearching = false;
+            if (!err) {
+                $('.back-layer').hide();
+                $('.dummy-element').css({'height': ($('body').height() - 35) + 'px'});
+                setMarkers(results);
+            }
+        });
+    }
 
-	$scope.showMarker = function (index) {
-		if (markers[index]) {
-			GoogleMap.event.trigger(markers[index], 'click', false);
-			mapView.panTo(markers[index].position);
-		}
-	};
+    $scope.showMarker = function (index) {
+        if (markers[index]) {
+            GoogleMap.event.trigger(markers[index], 'click', false);
+            mapView.panTo(markers[index].position);
+        }
+    };
 
-	loadGoogleMapsAPI({key: 'AIzaSyAUhIqtIVo154vh0lg0dFIHh-h5MBjFgUE', timeout: 5000}).then(function (mapInstance) {
-		GoogleMap = mapInstance;
-		initializeMap();
-	}).catch(function () {
-		Notifier.notify('Unable to load maps', 'try reloading page', 'error');
-	});
+    loadGoogleMapsAPI({key: 'AIzaSyAUhIqtIVo154vh0lg0dFIHh-h5MBjFgUE', timeout: 5000}).then(function (mapInstance) {
+        GoogleMap = mapInstance;
+        initializeMap();
+    }).catch(function () {
+        Notifier.notify('Unable to load maps', 'try reloading page', 'error');
+    });
 
-	inputListener();
+    inputListener();
 }
